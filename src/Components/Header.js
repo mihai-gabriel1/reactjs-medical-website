54fe94073ceda7706908dec3acc812f1b76f3316
mihai-gabriel1 <98354641+mihai-gabriel1@users.noreply.github.com>
2023-08-27 22:46:19 +0300

    About us section styled

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,7 +1,18 @@
 import React from "react";
 
 const Header = () => {
-  return <div className="header-container"></div>;
+  return (
+    <div className="header-container">
+      <div className="header-h1-btn-wrapper">
+        <h1 className="header-h1">
+          Bun venit la AliCranta <br /> Medical Center
+        </h1>
+        <a href="#about" class="btn-get-started">
+          Află mai multe
+        </a>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
