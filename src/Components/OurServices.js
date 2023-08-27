bbd7182766698ebc9708e86c54ebcaa47ade1e93
mihai-gabriel1 <98354641+mihai-gabriel1@users.noreply.github.com>
2023-08-27 23:40:41 +0300

    our services done

--- src/Components/OurServices.js
+++ src/Components/OurServices.js
@@ -1,5 +1,9 @@
 import consult from "../assets/img/consult.jpeg";
 import fisa from "../assets/img/fisa-auto.jpeg";
+import pulsoximetrie from "../assets/img/pulsoximetrie.jpeg";
+import apnee from "../assets/img/apnee.webp";
+import spirometrie from "../assets/img/spirometrie.jpeg";
+import masaj from "../assets/img/masaj.jpeg";
 import React from "react";
 
 const OurServices = () => {
@@ -9,19 +13,55 @@ const OurServices = () => {
         Serviciile oferite de catre noi
       </h2>
       <div className="row text-center">
-        <div className="col-6">
-          <p>Constult medical pneumologic</p>
+        <div className="col-xl-6 col-lg-6 col-md-6 col-sm-12">
+          <p className="service-title">Consult medical pneumologic</p>
           <img
-            className="img-fluid rounded"
+            className="img-fluid rounded services-img"
             src={consult}
             alt="doctor's appointment"
           />
         </div>
-        <div className="col-6">
-          <p>Fișă auto și consult psihologic</p>
+        <div className="col-xl-6 col-lg-6 col-md-6 col-sm-12">
+          <p className="service-title">Fișă auto și consult psihologic</p>
           <img
-            className="img-fluid rounded"
+            className="img-fluid rounded services-img"
             src={fisa}
+            alt="driving's license medical exam"
+          />
+        </div>
+      </div>
+      <div className="row text-center">
+        <div className="col-xl-6 col-lg-6 col-md-6 col-sm-12">
+          <p className="service-title">Pulsoximetrie</p>
+          <img
+            className="img-fluid rounded services-img"
+            src={pulsoximetrie}
+            alt="doctor's appointment"
+          />
+        </div>
+        <div className="col-xl-6 col-lg-6 col-md-6 col-sm-12">
+          <p className="service-title">Consult apnee</p>
+          <img
+            className="img-fluid rounded services-img"
+            src={apnee}
+            alt="driving's license medical exam"
+          />
+        </div>
+      </div>
+      <div className="row text-center">
+        <div className="col-xl-6 col-lg-6 col-md-6 col-sm-12">
+          <p className="service-title">Spirometrie</p>
+          <img
+            className="img-fluid rounded services-img"
+            src={spirometrie}
+            alt="doctor's appointment"
+          />
+        </div>
+        <div className="col-xl-6 col-lg-6 col-md-6 col-sm-12">
+          <p className="service-title">Terapii complementare</p>
+          <img
+            className="img-fluid rounded services-img"
+            src={masaj}
             alt="driving's license medical exam"
           />
         </div>
