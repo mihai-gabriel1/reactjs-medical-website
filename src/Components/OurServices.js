d727b44a430c55659b18d12f70fe3ca9781c0916
mihai-gabriel1 <98354641+mihai-gabriel1@users.noreply.github.com>
2023-08-27 23:24:06 +0300

    started our services section

--- src/Components/OurServices.js
+++ src/Components/OurServices.js
@@ -1,7 +1,33 @@
+import consult from "../assets/img/consult.jpeg";
+import fisa from "../assets/img/fisa-auto.jpeg";
 import React from "react";
 
 const OurServices = () => {
-  return <div>OurServices</div>;
+  return (
+    <div className="container our-services">
+      <h2 className="our-services-h2 section-title">
+        Serviciile oferite de catre noi
+      </h2>
+      <div className="row text-center">
+        <div className="col-6">
+          <p>Constult medical pneumologic</p>
+          <img
+            className="img-fluid rounded"
+            src={consult}
+            alt="doctor's appointment"
+          />
+        </div>
+        <div className="col-6">
+          <p>Fișă auto și consult psihologic</p>
+          <img
+            className="img-fluid rounded"
+            src={fisa}
+            alt="driving's license medical exam"
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default OurServices;
