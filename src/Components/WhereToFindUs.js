51ab8566dbcdcbf82b7b794ee8a12b9c0effd8ca
mihai-gabriel1 <98354641+mihai-gabriel1@users.noreply.github.com>
2023-08-30 22:58:46 +0300

    findus section edit

--- src/Components/WhereToFindUs.js
+++ src/Components/WhereToFindUs.js
@@ -1,4 +1,4 @@
-import findus from "../assets/img/findus.avif"; // Import the image
+import findus from "../assets/img/findus.avif";
 import React from "react";
 
 const WhereToFindUs = () => {
@@ -14,27 +14,31 @@ const WhereToFindUs = () => {
               alt="person browsing google maps"
             />
           </div>
-          <div className="col-xl-6 col-lg-6 col-md-12 col-sm-12">
-            <p className="findus-p">
-              Pe langă centrul de la adresa{" "}
-              <span className="findus-span">
-                Leonte Anastasievici Nr. 11 corp B,
-              </span>
-              mai puteti beneficia de serviciile noastre in alte două locații.
-            </p>
+          <div className="col-md-6">
+            <div className="findus-card">
+              <p className="findus-p">
+                Pe lângă centrul de la adresa{" "}
+                <span className="findus-span">
+                  Leonte Anastasievici Nr. 11 corp B,
+                </span>{" "}
+                mai puteți beneficia de serviciile noastre în alte două locații.
+              </p>
+            </div>
           </div>
-          <div className="col-xl-6 col-lg-6 col-md-12 col-sm-12">
-            <p className="findus-p">
-              Una dintre acestea poate fi gasită la adresa Jilava,{" "}
-              <span className="findus-span">
-                str. Magnoliei Nr 1, Dispensar medical Jilava
-              </span>
-              , iar cea din urmă, la adresa{" "}
-              <span className="findus-span">
-                Islaz, str. Dunării nr. 71, jud Teleorman
-              </span>
-              .
-            </p>
+          <div className="col-md-6">
+            <div className="findus-card">
+              <p className="findus-p">
+                Una dintre acestea poate fi găsită la adresa Jilava,{" "}
+                <span className="findus-span">
+                  str. Magnoliei Nr 1, Dispensar medical Jilava
+                </span>
+                , iar cealaltă la adresa{" "}
+                <span className="findus-span">
+                  Islaz, str. Dunării nr. 71, jud Teleorman
+                </span>
+                .
+              </p>
+            </div>
           </div>
         </div>
       </h2>
