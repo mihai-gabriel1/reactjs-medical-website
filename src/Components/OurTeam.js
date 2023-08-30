b60f56ad34ee3c3faf4ea8fdd0dbdc0798ef47ac
mihai-gabriel1 <98354641+mihai-gabriel1@users.noreply.github.com>
2023-08-30 22:22:39 +0300

    carousel slider added for the our team section

--- src/Components/OurTeam.js
+++ src/Components/OurTeam.js
@@ -1,79 +1,83 @@
 import React from "react";
-import drali from "../assets/img/drali.webp"; // Import the image
-import branzei from "../assets/img/branzei.webp"; // Import the image
-import drdruga from "../assets/img/drdruga.jpeg"; // Import the image
-import popescu from "../assets/img/popescu.webp"; // Import the image
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+
+import drali from "../assets/img/drali.webp";
+import branzei from "../assets/img/branzei.webp";
+import drdruga from "../assets/img/drdruga.jpeg";
+import popescu from "../assets/img/popescu.webp";
+
+const TeamMember = ({ imageSrc, name, description }) => (
+  <div className="dr-member">
+    <img
+      className="img-fluid rounded dr-pic"
+      src={imageSrc}
+      alt={`A picture with ${name}`}
+    />
+    <div className="dr-description">
+      <p className="dr-name">{name}</p>
+      <div className="dr-paragraphs">
+        {description.map((paragraph, index) => (
+          <p key={index} className="dr-paragraph">
+            {paragraph}
+          </p>
+        ))}
+      </div>
+    </div>
+  </div>
+);
 
 const OurTeam = () => {
   return (
     <div className="container our-team">
       <h2 className="our-team-h2 section-title">Echipa noastră</h2>
-      <div className="row dr-row">
-        <div className="col-xl-6 col-lg-6 col-md-12 col-sm-12 dr-col">
-          <img
-            className="img-fluid rounded dr-pic"
-            src={drali}
-            alt="A picture with dr ali cranta"
-          />
-        </div>
-        <div className="col-xl-6 col-lg-6 col-md-12 col-sm-12 dr-description">
-          <p className="dr-name">Dr. Ali Cranta</p>
-          <p className="dr-title">MEDIC FONDATOR CLINICA</p>
-          <p className="dr-title">MEDIC PRIMAR PNEUMOLOG</p>
-          <p className="dr-title">
-            COMPETENȚĂ EXPLORĂRI FUNCTIONALE RESPIRATORII
-          </p>
-          <p className="dr-title">COMPETENȚĂ BRONHOSCOPIE</p>
-        </div>
-      </div>
-      <div className="row dr-row mt-4 mt-4">
-        <div className="col-xl-6 col-lg-6 col-md-12 col-sm-12 dr-col">
-          <img
-            className="img-fluid rounded dr-pic"
-            src={branzei}
-            alt="A picture with dr popescu"
-          />
-        </div>
-        <div className="col-xl-6 col-lg-6 col-md-12 col-sm-12 dr-description">
-          <p className="dr-name">Dr. Brânzei Speranța Maria</p>
-          <p className="dr-title">MEDIC PRIMAR PNEUMOLOG</p>
-          <p className="dr-title">
-            COMPETENȚĂ EXPLORĂRI FUNCTIONALE RESPIRATORII
-          </p>
-          <p className="dr-title">COMPETENȚĂ BRONHOSCOPIE</p>
-        </div>
-      </div>
-      <div className="row dr-row mt-4">
-        <div className="col-xl-6 col-lg-6 col-md-12 col-sm-12 dr-col">
-          <img
-            className="img-fluid rounded dr-pic"
-            src={drdruga}
-            alt="A picture with dr druga"
-          />
-        </div>
-        <div className="col-xl-6 col-lg-6 col-md-12 col-sm-12 dr-description">
-          <p className="dr-name">Dr. Drugă Oana-Eufrosina</p>
-          <p className="dr-title">MEDIC SPECIALIST</p>
-          <p className="dr-title">GERIATRIE și GERONTOLOGIE</p>
-        </div>
-      </div>
-      <div className="row dr-row mt-4">
-        <div className="col-xl-6 col-lg-6 col-md-12 col-sm-12 dr-col">
-          <img
-            className="img-fluid rounded dr-pic"
-            src={popescu}
-            alt="A picture with ms popescu"
-          />
-        </div>
-        <div className="col-xl-6 col-lg-6 col-md-12 col-sm-12 dr-description">
-          <p className="dr-name">Popescu Cornelia</p>
-          <p className="dr-title">CONSILIER THETA HEALING</p>
-          <p className="dr-title">
-            Descoperiți mai multe despre terapia Theta accesând următorul link -
-            <a href="https://vindecareprinterapiatheta.ro/">Theta Healing</a>
-          </p>
-        </div>
-      </div>
+      <Carousel
+        showThumbs={false}
+        infiniteLoop={true}
+        autoPlay={true}
+        interval={3000}
+        className="custom-carousel"
+      >
+        <TeamMember
+          imageSrc={drali}
+          name="Dr. Ali Cranta"
+          description={[
+            "MEDIC FONDATOR CLINICA",
+            "MEDIC PRIMAR PNEUMOLOG",
+            "COMPETENȚĂ EXPLORĂRI FUNCTIONALE RESPIRATORII",
+            "COMPETENȚĂ BRONHOSCOPIE",
+          ]}
+        />
+        <TeamMember
+          imageSrc={branzei}
+          name="Dr. Brânzei Speranța Maria"
+          description={[
+            "MEDIC PRIMAR PNEUMOLOG",
+            "COMPETENȚĂ EXPLORĂRI FUNCTIONALE RESPIRATORII",
+            "COMPETENȚĂ BRONHOSCOPIE",
+          ]}
+        />
+        <TeamMember
+          imageSrc={drdruga}
+          name="Dr. Drugă Oana-Eufrosina"
+          description={["MEDIC SPECIALIST", "GERIATRIE și GERONTOLOGIE"]}
+        />
+        <TeamMember
+          imageSrc={popescu}
+          name="Popescu Cornelia"
+          description={[
+            "CONSILIER THETA HEALING",
+            "Descoperiți mai multe despre terapia Theta accesând următorul link -",
+            <a
+              href="https://vindecareprinterapiatheta.ro/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Theta Healing
+            </a>,
+          ]}
+        />
+      </Carousel>
     </div>
   );
 };
