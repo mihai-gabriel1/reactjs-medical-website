abf42ce976ba44cc36f03587ecd5cf60ca949e5e
Mihai Gabriel <mr.eu21@gmail.com>
2024-01-23 23:56:13 +0200

    prices section update; images generated

--- src/Components/Prices.js
+++ src/Components/Prices.js
@@ -1,7 +1,13 @@
 import React from "react";
 import priceLogo from "../assets/img/prices-img-section.jpeg";
+import apnee from "../assets/pricesServicesImgs/consult-apnee.jpeg";
+import consult from "../assets/pricesServicesImgs/consult-pneumologic.jpeg";
+import auto from "../assets/pricesServicesImgs/fisa-auto.jpeg";
+import pulsoximetrie from "../assets/pricesServicesImgs/pulsoximetrie.jpeg";
+import spirometrie from "../assets/pricesServicesImgs/spirometrie.jpeg";
+import theta from "../assets/pricesServicesImgs/terapii-complementare.jpeg";
 
-const Careers = () => {
+const Prices = () => {
   return (
     <div className="prices-section-wrapper container">
       <div className="price-logo-wrapper">
@@ -21,8 +27,40 @@ const Careers = () => {
         Descoperiți oferta noastră și alegeți îngrijirea de calitate la prețuri
         competitive:
       </p>
+      <div className="services-prices-info row">
+        <div className="col price-service">
+          <img className="price-service-img" src={apnee} alt="" />
+          <p className="prices-service-title">Consult apnee</p>
+          <p className="price-service-amount">Pret - 150 RON</p>
+        </div>
+        <div className="col price-service">
+          <img className="price-service-img" src={consult} alt="" />
+          <p className="prices-service-title">Consult pneumologic</p>
+          <p className="price-service-amount">Pret - 150 RON</p>
+        </div>
+        <div className="col price-service">
+          <img className="price-service-img" src={auto} alt="" />
+          <p className="prices-service-title">Consult pneumologic</p>
+          <p className="price-service-amount">Pret - 150 RON</p>
+        </div>
+        <div className="col price-service">
+          <img className="price-service-img" src={pulsoximetrie} alt="" />
+          <p className="prices-service-title">Consult pneumologic</p>
+          <p className="price-service-amount">Pret - 150 RON</p>
+        </div>
+        <div className="col price-service">
+          <img className="price-service-img" src={spirometrie} alt="" />
+          <p className="prices-service-title">Consult pneumologic</p>
+          <p className="price-service-amount">Pret - 150 RON</p>
+        </div>
+        <div className="col price-service">
+          <img className="price-service-img" src={theta} alt="" />
+          <p className="prices-service-title">Consult pneumologic</p>
+          <p className="price-service-amount">Pret - 150 RON</p>
+        </div>
+      </div>
     </div>
   );
 };
 
-export default Careers;
+export default Prices;
