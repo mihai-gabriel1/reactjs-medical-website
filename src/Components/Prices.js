760de7a5b9d10a1edc3a4b07dda834996079b078
Mihai Gabriel <mr.eu21@gmail.com>
2024-01-25 22:25:25 +0200

    prices section wrap up

--- src/Components/Prices.js
+++ src/Components/Prices.js
@@ -6,8 +6,103 @@ import auto from "../assets/pricesServicesImgs/fisa-auto.jpeg";
 import pulsoximetrie from "../assets/pricesServicesImgs/pulsoximetrie.jpeg";
 import spirometrie from "../assets/pricesServicesImgs/spirometrie.jpeg";
 import theta from "../assets/pricesServicesImgs/terapii-complementare.jpeg";
+import adeverinta from "../assets/pricesServicesImgs/adeverinta.jpeg";
+import aging from "../assets/pricesServicesImgs/anti-aging.webp";
+import cognitiva from "../assets/pricesServicesImgs/cognitiva.jpeg";
+import ekg from "../assets/pricesServicesImgs/ekg.jpeg";
+import handicap from "../assets/pricesServicesImgs/handicap.jpeg";
+import intravenos from "../assets/pricesServicesImgs/intravenos.jpeg";
+import testare from "../assets/pricesServicesImgs/testare-injectie.jpeg";
 
 const Prices = () => {
+  const services = [
+    {
+      name: "Consult inițial pneumologie",
+      price: "170 RON",
+      img: consult,
+    },
+    {
+      name: "Consult inițial geriatrie și evaluare cognitivă",
+      price: "140 RON",
+      img: cognitiva,
+    },
+    {
+      name: "Consult și evaluare anti-aging",
+      price: "140 RON",
+      img: aging,
+    },
+    {
+      name: "Control pneumologie",
+      price: "100 RON",
+      img: consult,
+    },
+    {
+      name: "Control geriatrie",
+      price: "80 RON",
+      img: consult,
+    },
+    {
+      name: "Spirometrie simplă",
+      price: "70 RON",
+      img: spirometrie,
+    },
+    {
+      name: "Spirometrie cu bronhodilatator",
+      price: "80 RON",
+      img: apnee,
+    },
+    {
+      name: "Pulsoximetrie",
+      price: "10 RON",
+      img: pulsoximetrie,
+    },
+    {
+      name: "Peak-flowmetrie",
+      price: "10 RON",
+      img: pulsoximetrie,
+    },
+    {
+      name: "EKG",
+      price: "30 RON",
+      img: ekg,
+    },
+    {
+      name: "Testare subcutanată la medicamente",
+      price: "20 RON",
+      img: testare,
+    },
+    {
+      name: "Injecție intramusculară",
+      price: "30 RON",
+      img: intravenos,
+    },
+    {
+      name: "Injecție intravenos",
+      price: "50 RON",
+      img: intravenos,
+    },
+    {
+      name: "Adeverință medicală A5",
+      price: "50 RON",
+      img: adeverinta,
+    },
+    {
+      name: "Referat handicap",
+      price: "50 RON",
+      img: handicap,
+    },
+    {
+      name: "Adeverință medicală simplă",
+      price: "50 RON",
+      img: theta,
+    },
+    {
+      name: "Consult pneumologic fișă auto profesioniști",
+      price: "50 RON",
+      img: auto,
+    },
+  ];
+
   return (
     <div className="prices-section-wrapper container">
       <div className="price-logo-wrapper">
@@ -28,68 +123,19 @@ const Prices = () => {
         competitive:
       </p>
       <div className="services-prices-info row">
-        <div className="col price-service">
-          <img className="price-service-img" src={apnee} alt="consult apnee" />
-          <div className="price-service-amount-wrapper">
-            <p className="prices-service-title">Consult apnee</p>
-            <p className="price-service-amount">Pret - 150 RON</p>
+        {services.map((service, index) => (
+          <div key={index} className="col price-service">
+            <img
+              className="price-service-img"
+              src={service.img}
+              alt={service.name}
+            />
+            <div className="price-service-amount-wrapper">
+              <p className="prices-service-title">{service.name}</p>
+              <p className="price-service-amount">Pret - {service.price}</p>
+            </div>
           </div>
-        </div>
-        <div className="col price-service">
-          <img
-            className="price-service-img"
-            src={consult}
-            alt="consult pneumologic"
-          />
-          <div className="price-service-amount-wrapper">
-            <p className="prices-service-title">Consult pneumologic</p>
-            <p className="price-service-amount">Pret - 150 RON</p>
-          </div>
-        </div>
-        <div className="col price-service">
-          <img
-            className="price-service-img"
-            src={auto}
-            alt="logo examen fisa auto"
-          />
-          <div className="price-service-amount-wrapper">
-            <p className="prices-service-title">Examen Fişǎ Auto</p>
-            <p className="price-service-amount">Pret - 150 RON</p>
-          </div>
-        </div>
-        <div className="col price-service">
-          <img
-            className="price-service-img"
-            src={pulsoximetrie}
-            alt="logo examen pulsoximetrie"
-          />
-          <div className="price-service-amount-wrapper">
-            <p className="prices-service-title">Examen de Pulsoximetrie</p>
-            <p className="price-service-amount">Pret - 150 RON</p>
-          </div>
-        </div>
-        <div className="col price-service">
-          <img
-            className="price-service-img"
-            src={spirometrie}
-            alt="logo examen spirometrie"
-          />
-          <div className="price-service-amount-wrapper">
-            <p className="prices-service-title">Examen de Spirometrie</p>
-            <p className="price-service-amount">Pret - 70 RON</p>
-          </div>
-        </div>
-        <div className="col price-service">
-          <img
-            className="price-service-img"
-            src={theta}
-            alt="terapii complementare"
-          />
-          <div className="price-service-amount-wrapper">
-            <p className="prices-service-title">Terapii Complementare</p>
-            <p className="price-service-amount">Pret - 150 RON</p>
-          </div>
-        </div>
+        ))}
       </div>
     </div>
   );
